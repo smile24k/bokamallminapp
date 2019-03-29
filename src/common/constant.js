6e1c599234204099cb8acc7cc9db6d12c9e4efc2
王同亮 <2355939251@qq.com>
2019-03-29 17:34:53 +0800

    腾讯视频init

--- src/common/constant.js
+++ src/common/constant.js
@@ -1,7 +1,8 @@
 const constant = {
   apiUrl: "https://api.bokao2o.com",
   testUrl: "https://test.bokao2o.com",
-  nodeUrl: "https://m.bokao2o.com"
+  nodeUrl: "https://m.bokao2o.com",
+  doubanApi:"https://api.douban.com"
 }
 
 export default constant;
